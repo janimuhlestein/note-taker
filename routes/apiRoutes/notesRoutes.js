05a9c9247363c13b69c43f571356f7f0c36152ef
Jani Muhlestein <janimuhlestein@gmail.com>
2020-10-08 17:53:17 -0600

    removed excess code

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {findById, createNewNote, validateNote, findByTitle, findByText} = require('../../lib/notes');
+const {createNewNote, validateNote, deleteNote} = require('../../lib/notes');
 const {notes} = require('../../Develop/db/db.json');
 
 console.log(notes);
@@ -7,33 +7,6 @@ console.log(notes);
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
-});
-
-router.get('/notes/:id', (req, res) => {
-    const result = findById(req.params.id, notes);
-    if(result) {
-        res.json(result);
-    } else{
-        res.status(404).send("I'm sorry, but no note with that Id exists");
-    }
-});
-
-router.get('/notes/:title', (req, res) => {
-    const result = findByTitle(req.params.title, notes) 
-        if(result) {
-            res.json(result);
-        } else {
-            res.status(404).send("I'm sorry,, but no note with that title exists");
-        }
-});
-
-router.get('/notes/:text', (req, res) => {
-    const result = findByText(req.params.text, {notes});
-    if(result) {
-        res.json(result);
-    } else {
-        res.status(404).send("I'm sorry, but no note with that text exists.");
-    }
 });
 
 router.post('/notes', (req, res) => {
@@ -48,7 +21,8 @@ router.post('/notes', (req, res) => {
 });
 
 router.delete('/notes/:id', (req, res)=> {
-    console.log(req.body);
+   deleteNote(req.params.id, notes);
+   console.log(req);
 
 });
 
