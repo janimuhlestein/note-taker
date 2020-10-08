ea714d6e5a7f230a334838aff2ab7fd0d7a57d64
Jani Muhlestein <janimuhlestein@gmail.com>
2020-10-08 12:20:07 -0600

    path fixes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router();
 const {findById, createNewNote, validateNote, findByTitle, findByText} = require('../../lib/notes');
 const {notes} = require('../../Develop/db/db.json');
-//const { setPriority } = require('os');
 
-router.get('notes', (req, res) => {
+console.log(notes);
+
+router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
 });
 
-router.get('notes/:id', (req, res) => {
+router.get('/notes/:id', (req, res) => {
     const result = findById(req.params.id, notes);
     if(result) {
         res.json(result);
@@ -17,7 +18,7 @@ router.get('notes/:id', (req, res) => {
     }
 });
 
-router.get('notes/:title', (req, res) => {
+router.get('/notes/:title', (req, res) => {
     const result = findByTitle(req.params.title, notes) 
         if(result) {
             res.json(result);
@@ -26,8 +27,8 @@ router.get('notes/:title', (req, res) => {
         }
 });
 
-router.get('notes/:text', (req, res) => {
-    const result = findByText(req.params.text, notes);
+router.get('/notes/:text', (req, res) => {
+    const result = findByText(req.params.text, {notes});
     if(result) {
         res.json(result);
     } else {
@@ -37,12 +38,18 @@ router.get('notes/:text', (req, res) => {
 
 router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
+    //console.log(req.body);
     if(!validateNote(req.body)) {
-        res.status(404).send("The note is not properly formatted.");
+       res.status(404).send("The note is not properly formatted.");
     } else{
         const note = createNewNote(req.body, notes);
         res.json(note);
-    }
+    } 
+});
+
+router.delete('/notes/:id', (req, res)=> {
+    console.log(req.body);
+
 });
 
 module.exports = router;
